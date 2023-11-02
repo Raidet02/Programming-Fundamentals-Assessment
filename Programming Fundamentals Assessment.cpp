45c2a35587ee1a2ef3fc3630e6971f37eb0d4df2
Raidet02 <146941600+Raidet02@users.noreply.github.com>
2023-11-02 16:38:24 +0000

    completed up to challenge 5

--- Programming Fundamentals Assessment.cpp
+++ Programming Fundamentals Assessment.cpp
@@ -2,18 +2,79 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+void textBoxGenerator(string& textForTextBox);
 
 int main() //TEXT FIGHT
 {
-    cout << " _______   ______   _    _   _______         ___     _______   _______      ___               \n";
-    cout << "|__   __| |  ____| \\ \\  / / |__   __|       / _ \\   |__   __| |__   __|    / _ \\                      \n";
-    cout << "   | |    | |____   \\ \\/ /     | |         / / \\ \\     | |       | |      / / \\ \\         \n";
-    cout << "   | |    |  ____|  |    |     | |        / /___\\ \\    | |       | |     / /___\\ \\  \n";
-    cout << "   | |    | |____   / /\\ \\     | |       /  _____  \\   | |       | |    /  _____  \\        \n";
-    cout << "   |_|    |______| /_/  \\_\\    |_|      /_/       \\_\\  |_|       |_|   /_/       \\_\\           \n";
+    /*cout << " _______   ______   _    _   _______         ___     _______   _______     ___       ______    _   _\n";
+    cout << "|__   __| |  ____| \\ \\  / / |__   __|       / _ \\   |__   __| |__   __|   / _ \\     /  ____|  | | / /\n";
+    cout << "   | |    | |____   \\ \\/ /     | |         / / \\ \\     | |       | |     / / \\ \\    | |       | |/ /\n";
+    cout << "   | |    |  ____|  |    |     | |        / /___\\ \\    | |       | |    / /___\\ \\   | |       | | |\n";
+    cout << "   | |    | |____   / /\\ \\     | |       /  _____  \\   | |       | |   /  _____  \\  | |____   | |\\ \\\n";
+    cout << "   |_|    |______| /_/  \\_\\    |_|      /_/       \\_\\  |_|       |_|  /_/       \\_\\ \\______|  |_| \\_\\\n";*/
+
+    /*string name = ""; //Initialize the variables that are going to be used
+    string clanTag = "";
+    string userName = "";
+    int age = 0;
+
+    cout << "Please enter your name: "; //Ask the user for their information and assign it to appropriate variables
+    cin >> name;
+
+    cout << "Please enter your clan tag: ";
+    cin >> clanTag;
+
+    cout << "Please enter your username: ";
+    cin >> userName;
+
+    cout << "Please enter your age: ";
+    cin >> age;
+
+    cout << "- Name: " << name << endl; //Out put the information in the appropriate manner 
+    cout << "- Username: " << "[" << clanTag << "] " << userName << endl;
+    cout << "- Age: " << age << endl;*/
+
+    /*float numToSquare = 0.0f; //Initialize the variable to be used
+
+    cout << "What number do you want to Square?"; //Ask the user for the umber they wish to be squared
+    cin >> numToSquare;
+
+    cout << "> " << numToSquare << " squared is " << numToSquare * numToSquare << endl; //Output the number in a clear manner*/
+
+    /*string textForTextBox = "";
+
+    cout << "Please enter some text: ";
+    getline(cin, textForTextBox);
+
+    textBoxGenerator(textForTextBox);*/
 
 
 }
+
+/*void textBoxGenerator(string& textForTextBox)
+{
+    int amountOfAsterisk = 0;
+
+    amountOfAsterisk = textForTextBox.length() + 4;
+
+    while (amountOfAsterisk >= 1)
+    {
+        cout << "*";
+        amountOfAsterisk--;
+    }
+
+    cout << endl << "* " << textForTextBox << " *" << endl;
+
+    amountOfAsterisk = textForTextBox.length() + 4;
+
+    while (amountOfAsterisk >= 1)
+    {
+        cout << "*";
+        amountOfAsterisk--;
+    }
+}*/
 
 
